e88937c12a7e3c1863926caa9ee170125b578e55
Samarpitaa <81858442+Samarpitaa@users.noreply.github.com>
2023-07-02 14:34:58 +0530

    Add files via upload

---
+++ Myclock/script.js
@@ -0,0 +1,36 @@
+function showTime() {
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session;
+
+  var day = date.getDate(); // 1 - 31
+  var month = date.getMonth() + 1; // 0 - 11 (add 1 to match standard month numbering)
+  var year = date.getFullYear();
+
+  var currentDate = month + "/" + day + "/" + year;
+
+  var dateTime = currentDate + "<br>"+ time;
+
+  document.getElementById("MyClockDisplay").innerHTML = dateTime;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
